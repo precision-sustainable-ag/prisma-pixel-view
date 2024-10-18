5d982f91d1136ce48ca4deef908001d6afd0db2b
brianwdavis <brianwdavis@gmail.com>
2024-10-18 10:44:04 -0400

    bugfix part 2

--- global.R
+++ global.R
@@ -96,11 +96,11 @@ bnorm <- function(x, normalize = T) {
 }
 
 lowest_dense_corner <- function(x, y) {
-  x = x[!is.na(x) & !is.na(y)]
-  y = y[!is.na(x) & !is.na(y)]
+  x_ = x[!is.na(x) & !is.na(y)]
+  y_ = y[!is.na(x) & !is.na(y)]
   
-  x_ = cut(x, 3, labels = c("l", "middle", "r"))
-  y_ = cut(y, 3, labels = c("b", "middle", "t"))
+  x_ = cut(x_, 3, labels = c("l", "middle", "r"))
+  y_ = cut(y_, 3, labels = c("b", "middle", "t"))
   combs = paste0(y_, x_) %>% 
     stringr::str_subset("middle", negate = T) %>% 
     forcats::fct(levels = c("tr", "tl", "br", "bl"))
